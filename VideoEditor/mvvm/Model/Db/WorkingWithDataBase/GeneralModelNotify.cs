a616b3e045b88bbcde2d8e7ab577cb77c48c529b
Tuchka <krusindanil01@gmail.com>
2023-08-21 17:39:54 +0700

    Update GeneralModelNotify.cs

--- VideoEditor/mvvm/Model/Db/WorkingWithDataBase/GeneralModelNotify.cs
+++ VideoEditor/mvvm/Model/Db/WorkingWithDataBase/GeneralModelNotify.cs
@@ -9,13 +9,13 @@ using VideoEditor.mvvm.Model.WorkingWithFile.Interface;
 
 namespace VideoEditor.mvvm.Model.Db.WorkingWithDataBase
 {
-	public class GeneralModelNotify<T> : GeneralModel<T>, INotifyDeleteFile where T : GeneralTable<T>, new()
+	public class GeneralModelNotify<T> : GeneralModel<T>, INotifyDeleteFile where T : GeneralTable<T>, new() // Общая модель уведомления действий с файлом
 	{
 		public GeneralModelNotify(ConnactDb connact) : base(connact)
 		{
 		}
 
-		public void OnFileDelete(GeneralFile file)
+		public void OnFileDelete(GeneralFile file) // Удаление файла
 		{
 			DeleteById(file.Id);
 		}
